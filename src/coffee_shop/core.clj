c6befd9b15e7e74567673105d2827f64959fe510
dawguy <dawguy>
2023-04-12 10:46:10 -0400

    Coffee shop no async.

--- src/coffee_shop/core.clj
+++ src/coffee_shop/core.clj
@@ -1,5 +1,8 @@
 (ns coffee-shop.core
-  (:require [clojure.core.async :refer :all]))
+  (:require [clojure.core.async :refer [>! <! >!! <!! take! put! chan offer! poll!]]))
+
+(def grind-time 100)
+(def brew-time 250)
 
 
 ; Paths to do this.
@@ -12,3 +15,28 @@
 ; a -> b -> c -> done!
 ;
 ; Extra things to consider. How would programming a worker to move the items from step to step look like?
+
+(defn create-grinder []
+  (fn [in]
+    (. Thread sleep grind-time)
+    (assoc in :state "ground")))
+
+(defn create-brewer []
+  (fn [in]
+    (. Thread sleep brew-time)
+    (assoc in :state "fresh-coffee")))
+
+(defn run-sim [n]
+  (let [grinder (create-grinder)
+        brewer (create-brewer)
+        orders (clojure.core/take n (range))]
+    (loop [[c & rem] orders
+           coffees []]
+      (if (nil? c)
+               coffees
+               (do
+                 (if (= 0 (mod c 10)) (prn (str "Processing: " c)))
+                 (recur rem (conj coffees (-> {:state "order-placed" :c c}
+                                              (grinder)
+                                              (brewer)))))))))
+(run-sim 50)
