bf42409d518e3fea8c855495b077482fd5ee5c47
dawguy <dawguy>
2023-04-12 16:19:57 -0400

    Parallelized machines in a coffee shop.

--- src/coffee_shop/core.clj
+++ src/coffee_shop/core.clj
@@ -5,18 +5,7 @@
 (def brew-time 250)
 (def table-size 100)
 (def timeout-length 2000)
-
-
-; Paths to do this.
-;
-; Pipeline
-; Fan-out
-; Fan-in
-;
-; Chained set of machines. All of each step sharing a set of in-out a/channels
-; a -> b -> c -> done!
-;
-; Extra things to consider. How would programming a worker to move the items from step to step look like?
+(def coffees-to-make 250)
 
 (defn my-prn [i s]
   (if (= 0 (mod i 10))
@@ -26,8 +15,9 @@
   (let [in-c (a/chan)
         out-c (a/chan)
         f (fn [] "Starts the grinder"
-          (a/go (loop [i 0]
-                (let [[v c] (a/alts! in-c (a/timeout timeout-length))]
+            (println (str "Starting grinder " name))
+            (a/go (loop [i 0]
+                (let [[v c] (a/alts! [in-c (a/timeout timeout-length)])]
                   (if (= c in-c)
                     (do
                       (my-prn i (str "Grinder: " name " handled " v))
@@ -41,12 +31,13 @@
                     )))))]
     [f in-c out-c]))
 
-(defn create-grinder [name]
+(defn create-brewer [name]
   (let [in-c (a/chan)
         out-c (a/chan)
-        f (fn [] "Starts the grinder"
+        f (fn [] "Starts the brewer"
+            (println (str "Starting brewer " name))
             (a/go (loop [i 0]
-                  (let [[v c] (a/alts! in-c (a/timeout timeout-length))]
+                  (let [[v c] (a/alts! [in-c (a/timeout timeout-length)])]
                     (if (= c in-c)
                       (do
                         (my-prn i (str "Brewer: " name " handled " v))
@@ -61,7 +52,7 @@
     [f in-c out-c]))
 
 (defn create-table [in-chans]
-  [nil nil (a/merge in-chans table-size)])
+  (a/merge in-chans table-size))
 
 (defn ->v [vals position]
   (mapv #(nth % position) vals)
@@ -69,29 +60,41 @@
 
 (defn run-sim [n]
   (let [grinders (map #(create-grinder %) ["g-1" "g-2"])
-        brewers (map #(create-grinder %) ["b-1" "b-2"])
-        grinders-f (->v grinders 0)
-        brewers-f (->v brewers 0)
-        grinders-in (->v grinders 1)
-        brewers-in (->v brewers 1)
+        brewers (map #(create-brewer %) ["b-1" "b-2"])
         grinders-out (create-table (->v grinders 2))
         brewers-out (create-table (->v brewers 2))
         orders (clojure.core/take n (range))]
-    (doseq [b brewers-f] (b))
-    (doseq [g grinders-f] (g))
+    (doseq [g (->v grinders 0)] (g))
+    (doseq [b (->v brewers 0)] (b))
+    (a/go-loop []
+      (let [in-v (a/<! grinders-out)]
+        (if (nil? in-v)
+          brewers-out
+          (let [[v port] (a/alts! (conj (mapv #(vector % in-v) (->v brewers 1))
+                                    (a/timeout timeout-length)))]
+          (if (= v true)
+            (recur)
+            (do (println "Timeout hit for brewer input")
+                brewers-out))))))
     (a/go-loop [[c & rem] orders]
       (if (nil? c)
         brewers-out
         (do
            (my-prn c (str "Processing: " c))
            (let [placed {:state "order-placed" :c c}
-                [v port] (a/alts! (conj (mapv #(vector % placed) grinders-in)
+                [v port] (a/alts! (conj (mapv #(vector % placed) (->v grinders 1))
                                         (a/timeout timeout-length)))]
             (if (= v true)
               (recur rem)
               (do (println "Timeout hit for input")
-                  brewers-out))))))))
+                  brewers-out))))))
+    brewers-out))
 
-(a/go
-  (let [out-c (run-sim 50)]
-    (println (str "Output: " (a/<! out-c)))))
+(let [out-c (run-sim coffees-to-make)]
+  (a/go-loop [coffees []]
+    (let [[v port] (a/alts! [out-c (a/timeout timeout-length)])]
+      (if (nil? v)
+        (do
+          (prn coffees)
+          coffees)
+        (recur (conj coffees v))))))
